3f6c329bb59a476028714210b2e6e119b4706a4d
abinash <dev,abinashshrestha@gmail.com>
2024-05-16 11:18:00 +0545

    redirected page

--- failed.php
+++ failed.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php
+require('admin/inc/essentials.php');
+require('admin/inc/db_config.php');
 
-<body>
-    <h1>Failed</h1>
-</body>
-
-</html>
+redirect("index.php");
+?>
